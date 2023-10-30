52a7b5dc86f9ebe928a50cf219cf1a025546874e
rianwp <rianwp2015@gmail.com>
2023-10-30 22:01:27 +0700

    refactor to react context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,41 @@
-import { useRef } from 'react';
+import { useContext, useRef } from "react"
 
-import CartModal from './CartModal.jsx';
+import CartModal from "./CartModal.jsx"
+import { CartContext } from "../store/shopping-cart-context.js"
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
-  const modal = useRef();
+export default function Header() {
+	const { items } = useContext(CartContext)
+	const modal = useRef()
 
-  const cartQuantity = cart.items.length;
+	const cartQuantity = items.length
 
-  function handleOpenCartClick() {
-    modal.current.open();
-  }
+	function handleOpenCartClick() {
+		modal.current.open()
+	}
 
-  let modalActions = <button>Close</button>;
+	let modalActions = <button>Close</button>
 
-  if (cartQuantity > 0) {
-    modalActions = (
-      <>
-        <button>Close</button>
-        <button>Checkout</button>
-      </>
-    );
-  }
+	if (cartQuantity > 0) {
+		modalActions = (
+			<>
+				<button>Close</button>
+				<button>Checkout</button>
+			</>
+		)
+	}
 
-  return (
-    <>
-      <CartModal
-        ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
-        title="Your Cart"
-        actions={modalActions}
-      />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
-      </header>
-    </>
-  );
+	return (
+		<>
+			<CartModal ref={modal} title="Your Cart" actions={modalActions} />
+			<header id="main-header">
+				<div id="main-title">
+					<img src="logo.png" alt="Elegant model" />
+					<h1>Elegant Context</h1>
+				</div>
+				<p>
+					<button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+				</p>
+			</header>
+		</>
+	)
 }
